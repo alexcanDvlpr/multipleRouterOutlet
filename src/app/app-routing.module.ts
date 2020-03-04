1880aa1719f8590b16fe8d832bb89622a54dd589
Alejandro Cantón García <acanton.ext@gmv.com>
2020-03-04 15:24:54 +0100

    multipleRouter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GetstartedComponent } from './components/getstarted/getstarted.component';
 import { LoginComponent } from './components/login/login.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 
 const routes: Routes = [
   { path: '', component: GetstartedComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'panel', component: HomeComponent }
+  { path: 'panel', component: HomeComponent, children: [
+    { path: 'characters', component: CharactersComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: '**', redirectTo: 'characters' },
+  ] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { GetstartedComponent } from './components/getstarted/getstarted.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    GetstartedComponent
+    GetstartedComponent,
+    CharactersComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
